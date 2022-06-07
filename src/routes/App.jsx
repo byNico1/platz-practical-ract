de58fd82592ba282e68514a65604b3eed0a2b660
bynico1 <nicofoque0205@gmail.com>
2022-06-07 08:50:20 -0500

    redeploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter>
+      <BrowserRouter basename="/platz-practical-ract">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
